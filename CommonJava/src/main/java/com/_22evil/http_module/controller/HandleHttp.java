ff40c0f3855b4a1ca02da1e522808e6a5b3e6b00
shangguyi <984370133@qq.com>
2018-07-02 17:38:35 +0800

    1. 增加了一个类扫描工具 2. 增加了一个简单的http module模块，可以直接使用

---
+++ CommonJava/src/main/java/com/_22evil/http_module/controller/HandleHttp.java
@@ -0,0 +1,12 @@
+package com._22evil.http_module.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HandleHttp {
+    String url();
+}
---
+++ CommonJava/src/main/java/com/_22evil/http_module/controller/TestController.java
@@ -0,0 +1,12 @@
+package com._22evil.http_module.controller;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+@HandleHttp(url = "/test")
+public class TestController extends BaseHttpController {
+    @Override
+    public void service(MockHttpServletRequest request, MockHttpServletResponse response) throws Exception{
+        response.getWriter().write("this is test");
+    }
+}
