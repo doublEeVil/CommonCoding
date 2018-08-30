7a8f953e3dbac2d042c02e18c7209df75438b158
shangguyi <984370133@qq.com>
2018-08-30 21:22:34 +0800

    1

--- CommonJava/src/test/java/com/_22evil/test/TestBasic.java
+++ CommonJava/src/test/java/com/_22evil/test/TestBasic.java
@@ -1,5 +1,8 @@
 package com._22evil.test;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +16,32 @@ public class TestBasic {
     @Test
     public void test1() {
         System.out.println("====");
+
+        URL url = this.getClass().getClassLoader().getResource("");
+        System.out.println("---" + url);
+        File file = new File(url.getFile());
+        System.out.println(file.getName());
     }
 
     public static void main(String[] args) {
         System.out.println("====");
+    }
+}
+
+class TT1 {
+    private String name;
+    private int age;
+
+    public TT1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
