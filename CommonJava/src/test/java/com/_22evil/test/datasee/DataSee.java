0aed78e83b5a52e6bae8cb0bb1249ab1b4dc3cd6
shangguyi <984370133@qq.com>
2018-12-04 19:09:42 +0800

    ruby学习笔记

--- CommonJava/src/test/java/com/_22evil/test/datasee/DataSee.java
+++ CommonJava/src/test/java/com/_22evil/test/datasee/DataSee.java
@@ -1,32 +1,26 @@
 package com._22evil.test.datasee;
 import com._22evil.util.HttpCallback;
 import com._22evil.util.HttpUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 public class DataSee {
 
-    public static void main(String ... args) {
+    public static void main(String ... args) throws Exception {
         new DataSee().see();
     }
 
-    private void see() {
+    private void see() throws Exception{
         // 1. weibo 热点
-        HttpUtil.get("https://s.weibo.com/top/summary", new HttpCallback() {
-            @Override
-            public void fail(String errorMsg) {
-                System.out.println("!!!获取微博热点失败");
-            }
-
-            @Override
-            public void success(int code, String data) {
-                System.out.println(data);
-                Pattern ptn = Pattern.compile("<[\\u4E00-\\u9FA5]>");
-                Matcher matcher = ptn.matcher(data);
-                while (matcher.find()) {
-                    System.err.println(matcher.group());
-                }
-            }
-        });
+        String weiboUrl = "https://s.weibo.com/top/summary";
+        Document doc = Jsoup.connect(weiboUrl).get();
+        System.out.println(doc);
+        Elements elements = doc.getElementsByClass("td-02");
+        System.err.println(elements.size());
+        for (Element element : elements) {
+            System.out.println(element.text() + " ");
+        }
     }
 }
--- CommonJava/src/test/java/com/_22evil/test/spark/TestHelloWorld.java
+++ CommonJava/src/test/java/com/_22evil/test/spark/TestHelloWorld.java
@@ -2,9 +2,7 @@ package com._22evil.test.spark;
 
 import org.junit.Test;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-
+import static spark.Spark.*;
 /**
  * 各种报错，实在是不清楚spark需要哪个javax.servlet版本
  *
@@ -14,9 +12,11 @@ public class TestHelloWorld {
     //@Test
     public static void main(String[] args) throws Exception{
         port(8888);
-        get("/", (req, resp) -> {
-            resp.body("dd");
-            return "hello world...";
+        post("/abc", (req, resp) -> {
+            resp.header("Access-Control-Allow-Origin", "*");
+            //resp.redirect("/abc");
+            //resp.redirect("/abc");
+            return "{status: 12}";
         });
         //Thread.sleep(11111);
     }
