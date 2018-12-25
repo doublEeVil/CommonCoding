f3a4782032d1ca1b68509444152dd6a802139496
shangguyi <984370133@qq.com>
2018-12-25 10:10:17 +0800

    日常coding

--- CommonJava/src/main/java/com/_22evil/singleton/Singleton1.java
+++ CommonJava/src/main/java/com/_22evil/singleton/Singleton1.java
@@ -3,6 +3,9 @@ package com._22evil.singleton;
 /**
  * 最简单的单例写法
  * 不推荐
+ *
+ * 问题所在：
+ *  不能保证线程安全
  */
 public class Singleton1 {
     private static Singleton1 instance;
--- CommonJava/src/main/java/com/_22evil/singleton/Singleton3.java
+++ CommonJava/src/main/java/com/_22evil/singleton/Singleton3.java
@@ -16,6 +16,12 @@ public class Singleton3 {
             synchronized(Singleton3.instance) {
                 if (null == instance) {
                     instance = new Singleton3();
+                    // 唯一存在的问题， synchronized不能保证块内部不会发生指令重排序
+                    // new Singleton3()不是原子操作
+                    // 具体过程可能是  1. 分配内存 2. 对象初始化 3. instance指向该内存
+                    // 也可能是       1. 分配内存 2. instance指向该对象 3. 对象初始化
+                    // 至于是否发生指令重排序，未知，
+                    // 所以，多线程条件下，有可能出现返回未初始化对象的错误
                 }
             }
         }
